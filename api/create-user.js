f67e2356d14e1bd80d854fb54c747d574aa7626e
Rob3rtG3bau3r <robert.gebauer@koeln.de>
2025-06-05 16:59:30 +0200

    Update create-user.js

--- api/create-user.js
+++ api/create-user.js
@@ -7,10 +7,11 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Vary', 'Origin');
   // ✅ Wichtig: Behandle die OPTIONS-Anfrage separat und gib frühzeitig zurück
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
