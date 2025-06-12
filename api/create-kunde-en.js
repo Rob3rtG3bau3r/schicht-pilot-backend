2e19b3dc0ba6535c6093a20df5b4d09fd741ea21
Rob3rtG3bau3r <robert.gebauer@koeln.de>
2025-06-12 13:18:15 +0200

    Update create-kunde-en.js

--- api/create-kunde-en.js
+++ api/create-kunde-en.js
@@ -5,7 +5,8 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Vary', 'Origin');
